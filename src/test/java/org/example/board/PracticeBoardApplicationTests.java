193dfbd29162690d6e54a156ee97f7d36e1d49da
arrilab <arrilab@gmail.com>
2025-11-21 18:54:05 +0900

    feat: #4-Spring Boot 프로젝트 시작
    의존성 추가
    -스프링 웹
    -액츄에이터
    -데브툴즈
    -롬복

---
+++ src/test/java/org/example/board/PracticeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
