566c1a59aeb29fdec812c2cc7d78160416406915
arrilab <arrilab@gmail.com>
2025-11-24 19:14:57 +0900

    fix: @DataJpaTest import 문제 해결
    잘못된 import 경로 지정한 것을 수정함

--- src/test/java/org/example/board/repository/JpaRepositoryTest.java
+++ src/test/java/org/example/board/repository/JpaRepositoryTest.java
@@ -5,7 +5,7 @@ import org.example.board.domain.Article;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
 import java.util.List;
