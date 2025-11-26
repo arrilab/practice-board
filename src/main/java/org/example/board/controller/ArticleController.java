be5114fd3b19b1269057717d88f4a17aee0ed2c5
arrilab <arrilab@gmail.com>
2025-11-26 14:05:19 +0900

    test: 게시글/게시판 관련 View 테스트 작성 (#33)
    DDD 방식으로 작업하기 위해 우선 테스트를 작성함
    - 빈 Controller 생성 후 Test 코드로 명세 작성
    - 이후 Controller 코드 구현 예정
    - 테스트 코드가 실패하는 것에 의해 'gradle build'이 실패함, 이는 배포 자동화에 악영향을 주므로 빌드 악영향을 최소화하기 위해 각 테스트마다 '@disabled'를 추가함

---
+++ src/main/java/org/example/board/controller/ArticleController.java
@@ -0,0 +1,16 @@
+package org.example.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * todo: Endpoint List
+ * /articles
+ * /articles/{article-id}
+ * /articles/search
+ * /articles/search-hashtag
+ * */
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+}
