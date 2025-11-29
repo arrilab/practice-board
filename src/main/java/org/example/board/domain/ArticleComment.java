5660c2daaf48641f48d609a65d1cbfcba0adaeb3
arrilab <arrilab@gmail.com>
2025-11-29 15:11:48 +0900

    feat: Domain 업데이트 - Article과 ArticleComment 연관관계 매핑

--- src/main/java/org/example/board/domain/ArticleComment.java
+++ src/main/java/org/example/board/domain/ArticleComment.java
@@ -8,49 +8,54 @@ import lombok.ToString;
 import java.util.Objects;
 
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table(indexes = {
         @Index(columnList = "content"),
         @Index(columnList = "createdAt"),
-        @Index(columnList = "createdBy"),
+        @Index(columnList = "createdBy")
 })
 @Entity
 public class ArticleComment extends AuditingFields {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;                    // 식별자
+    private Long id;
 
     @Setter
     @ManyToOne(optional = false)
-    private Article article;            // 게시글
+    private Article article; // 게시글 (ID)
+
+    @Setter
+    @ManyToOne(optional = false)
+    private UserAccount userAccount; // 유저 정보 (ID)
 
     @Setter
     @Column(nullable = false, length = 500)
-    private String content;             // 본문
+    private String content; // 본문
 
 
     protected ArticleComment() {
     }
 
-    public ArticleComment(Article article, String content) {
+    private ArticleComment(Article article, UserAccount userAccount, String content) {
         this.article = article;
+        this.userAccount = userAccount;
         this.content = content;
     }
 
-    public static ArticleComment of(Article article, String content) {
-        return new ArticleComment(article, content);
+    public static ArticleComment of(Article article, UserAccount userAccount, String content) {
+        return new ArticleComment(article, userAccount, content);
     }
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         ArticleComment that = (ArticleComment) o;
-        return id != null && Objects.equals(id, that.id);
+        return id != null && Objects.equals(getId(), that.getId());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(id);
+        return Objects.hashCode(getId());
     }
 }
