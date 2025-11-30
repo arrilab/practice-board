50015c2671e17b0eb5d84396e31f30c953f305fa
arrilab <arrilab@gmail.com>
2025-11-30 21:59:43 +0900

    feat: 게시판 Business Logic 구현 (#52)
    ArticleServiceTest를 토대로 게시판 서비스에 필요한 비즈니스 로직 구현 완료

--- src/main/java/org/example/board/service/ArticleService.java
+++ src/main/java/org/example/board/service/ArticleService.java
@@ -1,6 +1,9 @@
 package org.example.board.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.board.domain.Article;
 import org.example.board.domain.type.SearchType;
 import org.example.board.dto.ArticleDto;
 import org.example.board.dto.ArticleWithCommentsDto;
@@ -10,6 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Transactional
 @Service
@@ -18,22 +22,51 @@ public class ArticleService {
     private final ArticleRepository articleRepository;
 
     @Transactional(readOnly = true)
-    public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
-        return Page.empty();
+    public Page<ArticleDto> searchArticles(String searchKeyword, SearchType searchType, Pageable pageable) {
+        if (searchKeyword == null || searchKeyword.isBlank()) {
+            return articleRepository.findAll(pageable).map(ArticleDto::from);
+        }
+
+        return switch (searchType) {
+            case TITLE -> articleRepository.findByTitleContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case CONTENT -> articleRepository.findByContentContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case ID -> articleRepository.findByUserAccount_UserIdContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case NICKNAME -> articleRepository.findByUserAccount_NicknameContaining(searchKeyword, pageable).map(ArticleDto::from);
+            // TODO: 사용자가 #을 붙이는 등의 경우도 해결
+            case HASHTAG -> articleRepository.findByHashtag("#" + searchKeyword, pageable).map(ArticleDto::from);
+        };
     }
 
     @Transactional(readOnly = true)
     public ArticleWithCommentsDto getArticle(Long articleId) {
-        return null;
+        return articleRepository.findById(articleId)
+                .map(ArticleWithCommentsDto::from)
+                .orElseThrow(() -> new EntityNotFoundException("게시글이 없습니다 - articleId: " + articleId)); // 주의) 운영 로그에 회원 정보가 남는 문제 있음
     }
 
     public void saveArticle(ArticleDto dto) {
+        articleRepository.save(dto.toEntity());
     }
 
     public void updateArticle(ArticleDto dto) {
+        try {
+            Article article = articleRepository.getReferenceById(dto.id());
+
+            if (dto.title() != null) {
+                article.setTitle(dto.title());
+            }
+
+            if (dto.content() != null) {
+                article.setContent(dto.content());
+            }
+
+            article.setHashtag(dto.hashtag());
+        } catch (EntityNotFoundException e) {
+            log.warn("게시글 업데이트 실패: 게시글을 찾을 수 없음 dto={}", dto);
+        }
     }
 
     public void deleteArticle(long articleId) {
+        articleRepository.deleteById(articleId);
     }
 }
-
